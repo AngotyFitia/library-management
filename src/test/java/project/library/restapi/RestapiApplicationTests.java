88442a7c55b4a8ab6a4cae372ea9f81be394c6aa
Toky Raselison <toky.raselison@orchisintl.com>
2026-03-21 16:20:31 +0300

    test: configurer le profil de test avec properties dédiées
    Profil 'test' avec base H2 isolée et secret JWT identique.
    Permet de lancer les tests sans conflit avec la config principale.

--- src/test/java/project/library/restapi/RestapiApplicationTests.java
+++ src/test/java/project/library/restapi/RestapiApplicationTests.java
@@ -2,12 +2,14 @@ package project.library.restapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestapiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Vérifie que le contexte Spring démarre correctement
+    }
 }
