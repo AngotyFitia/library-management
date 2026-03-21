7188adda5fb02718977d704a523571de7e40b578
Toky Raselison <toky.raselison@orchisintl.com>
2026-03-21 16:18:19 +0300

    feat(repository): compléter UserRepository avec filtres par nom et rôle
    - findByEmail, existsByEmail
    - findByNomContainingIgnoreCase (pagination)
    - findByRole (pagination)
    - findByNomContainingIgnoreCaseAndRole (filtre combiné)

--- src/main/java/project/library/restapi/repository/UserRepository.java
+++ src/main/java/project/library/restapi/repository/UserRepository.java
@@ -1,9 +1,22 @@
 package project.library.restapi.repository;
-import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.library.restapi.model.Role;
 import project.library.restapi.model.User;
 
-public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByEmailAndPassword(String email,String password);
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Page<User> findByNomContainingIgnoreCase(String nom, Pageable pageable);
+
+    Page<User> findByRole(Role role, Pageable pageable);
+
+    Page<User> findByNomContainingIgnoreCaseAndRole(String nom, Role role, Pageable pageable);
 }
